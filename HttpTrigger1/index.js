6ea7c8dd62f4fd1c52569f480793f4398183d294
Premchander Satheesh <psathees@uwaterloo.ca>
2023-04-10 17:01:21 -0400

    clear git changes

--- HttpTrigger1/index.js
+++ HttpTrigger1/index.js
@@ -5,7 +5,9 @@ module.exports = async function (context, req) {
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
+    context.log("XYZ", context.bindings.myInputBlob);
+    context.log("ABC", context.bindings.myOutputBlob);
+    context.bindings.myOutputBlob = context.bindings.myInputBlob
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
